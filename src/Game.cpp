a33e1136d0884ba436dd070d575af65283720ec4
Dan <dan@devoured.io>
2024-03-03 14:36:41 -0400

    feat: renders surface to texture

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include <SDL2/SDL_image.h>
 #include <iostream>
 
 Game::Game() { std::cout << "Game constructor called!" << std::endl; }
@@ -10,17 +11,26 @@ void Game::Initialize() {
     std::cerr << "ERROR: could not initialize SDL." << std::endl;
     return;
   }
-  window =
-      SDL_CreateWindow("Roraima v1.0.0", SDL_WINDOWPOS_CENTERED,
-                       SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS);
+
+  SDL_DisplayMode displayMode;
+  SDL_GetCurrentDisplayMode(0, &displayMode);
+
+  windowWidth = displayMode.w;
+  windowHeight = displayMode.h;
+  window = SDL_CreateWindow("Roraima v1.0.0", SDL_WINDOWPOS_CENTERED,
+                            SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight,
+                            SDL_WINDOW_BORDERLESS);
   if (!window) {
     std::cerr << "ERROR: could not create SDL window." << std::endl;
   }
 
-  renderer = SDL_CreateRenderer(window, -1, 0);
+  renderer = SDL_CreateRenderer(
+      window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
   if (!renderer) {
     std::cerr << "ERROR: could not create SDL renderer." << std::endl;
   }
+
+  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 
   isRunning = true;
 }
@@ -40,16 +50,33 @@ void Game::ProcessInput() {
     }
   }
 }
+void Game::Setup() {
+  // TODO: Initialize game objects
+}
 
-void Game::Update() {}
+void Game::Update() {
+  // TODO: Update game objects
+}
 
 void Game::Render() {
-  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_SetRenderDrawColor(renderer, 21, 21, 21, 255);
   SDL_RenderClear(renderer);
+
+  // Draw rectangle
+  SDL_Surface *surface = IMG_Load("./assets/images/tank-tiger-right.png");
+  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
+  SDL_FreeSurface(surface);
+
+  SDL_Rect dstRect = {10, 10, 32, 32};
+
+  SDL_RenderCopy(renderer, texture, NULL, &dstRect);
+  SDL_DestroyTexture(texture);
+
   SDL_RenderPresent(renderer);
 }
 
 void Game::Run() {
+  Setup();
   while (isRunning) {
     ProcessInput();
     Update();
