6e266a7e3c87d65b8dfdd6c84dfe50a5b8bfb60b
Dan <dan@devoured.io>
2024-03-03 12:25:32 -0400

    feat: red screen

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,20 +5,60 @@ Game::Game() { std::cout << "Game constructor called!" << std::endl; }
 
 Game::~Game() { std::cout << "Game desconstructor called!" << std::endl; }
 
-void Game::Initialize() {}
+void Game::Initialize() {
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    std::cerr << "ERROR: could not initialize SDL." << std::endl;
+    return;
+  }
+  window =
+      SDL_CreateWindow("Roraima v1.0.0", SDL_WINDOWPOS_CENTERED,
+                       SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_BORDERLESS);
+  if (!window) {
+    std::cerr << "ERROR: could not create SDL window." << std::endl;
+  }
 
-void Game::ProcessInput() {}
+  renderer = SDL_CreateRenderer(window, -1, 0);
+  if (!renderer) {
+    std::cerr << "ERROR: could not create SDL renderer." << std::endl;
+  }
+
+  isRunning = true;
+}
+
+void Game::ProcessInput() {
+  SDL_Event sdlEvent;
+  while (SDL_PollEvent(&sdlEvent)) {
+    switch (sdlEvent.type) {
+    case SDL_QUIT:
+      isRunning = false;
+      break;
+    case SDL_KEYDOWN:
+      if (sdlEvent.key.keysym.sym == SDLK_ESCAPE) {
+        isRunning = false;
+      }
+      break;
+    }
+  }
+}
+
+void Game::Update() {}
+
+void Game::Render() {
+  SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+  SDL_RenderClear(renderer);
+  SDL_RenderPresent(renderer);
+}
 
 void Game::Run() {
-  while (true) {
+  while (isRunning) {
     ProcessInput();
     Update();
     Render();
   }
 }
 
-void Game::Update() {}
-
-void Game::Render() {}
-
-void Game::Destroy() {}
+void Game::Destroy() {
+  SDL_DestroyWindow(window);
+  SDL_DestroyRenderer(renderer);
+  SDL_Quit();
+}
