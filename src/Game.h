b6251d73f0280f595b44cc1661f48deec5fc91fc
Dan <dan@devoured.io>
2024-03-03 19:37:28 -0400

    feat: fps control setup

--- src/Game.h
+++ src/Game.h
@@ -3,11 +3,15 @@
 
 #include <SDL2/SDL.h>
 
+const int FPS = 60;
+const int MILLISECS_PER_FRAME = 1000 / FPS;
+
 class Game {
 private:
   SDL_Window *window;
   SDL_Renderer *renderer;
   bool isRunning;
+  int millisecsPreviousFrame;
 
 public:
   Game();
